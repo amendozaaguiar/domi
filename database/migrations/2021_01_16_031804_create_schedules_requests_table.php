7808d50ddfcbb1c9bdff9c6ecdef0e6521e9bf1c
Anderson Mendoza Aguiar <amendoza@inmov.com>
2021-01-15 22:56:06 -0500

    Se agregan los modelos necesarios

---
+++ database/migrations/2021_01_16_031804_create_schedules_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedule_requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('schedule_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            //llaves foraneas
+            $table->foreign('schedule_id')->references('id')->on('schedules');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedule_requests');
+    }
+}
---
+++ database/seeders/ScheduleSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Schedule;
+use DateTime;
+use Illuminate\Database\Seeder;
+
+class ScheduleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $time = new DateTime('08:00:00');
+
+        do
+        {
+            $schedule = new Schedule;
+                $schedule->time = $time->format('H:i:s');
+                $schedule->maximum_allocation = 8;
+            $schedule->save();
+
+            $time->modify('+30 minute');
+
+        }while($time->format('H:i:s') <= '20:00:00');
+    }
+}
